1e86b54ddd3cac5e9af92a6d3e97d83cd02a5398
Ardian <ardianfirmansyah123@gmail.com>
2022-06-27 15:16:00 +0700

    Update Layouting Tabel

--- application/views/admin/kendaraan/dataKendaraanall.php
+++ application/views/admin/kendaraan/dataKendaraanall.php
@@ -31,23 +31,27 @@
                          <?php endif; ?>
                      </div>
                      <div class="card-body">
-                         <table class="table table-bordered table-striped example width=" 100%" height="auto">
+                         <table class="table table-bordered table-striped example" width="100%" height="auto">
                              <thead>
                                  <tr>
-                                     <th class="text-center">No</th>
-                                     <th width="125px" class="text-center">Aksi</th>
-                                     <th class="text-center">ID Aset</th>
-                                     <th class="text-center">No. Polisi</th>
-                                     <th class="text-center">Jenis</th>
-                                     <th class="text-center">Merk</th>
-                                     <th class="text-center">Tipe</th>
-                                     <th class="text-center">No. STNK</th>
-                                     <th class="text-center">Masa Berlaku STNK</th>
-                                     <th class="text-center">No. Mesin</th>
-                                     <th class="text-center">No. Rangka</th>
-                                     <th class="text-center">Tahun Motor</th>
-                                     <th class="text-center">Jenis Bahan Bakar</th>
-                                     <th class="text-center">Besar CC</th>
+                                     <th class="text-center" rowspan="2">No</th>
+                                     <th class="text-center" colspan="2">Aksi</th>
+                                     <th class="text-center" rowspan="2">ID Aset</th>
+                                     <th class="text-center" rowspan="2">No. Polisi</th>
+                                     <th class="text-center" rowspan="2">Jenis</th>
+                                     <th class="text-center" rowspan="2">Merk</th>
+                                     <th class="text-center" rowspan="2">Tipe</th>
+                                     <th class="text-center" rowspan="2">No. STNK</th>
+                                     <th class="text-center" rowspan="2">Masa Berlaku STNK</th>
+                                     <th class="text-center" rowspan="2">No. Mesin</th>
+                                     <th class="text-center" rowspan="2">No. Rangka</th>
+                                     <th class="text-center" rowspan="2">Tahun Motor</th>
+                                     <th class="text-center" rowspan="2">Jenis Bahan Bakar</th>
+                                     <th class="text-center" rowspan="2">Besar CC</th>
+                                 </tr>
+                                 <tr>
+                                     <th></th>
+                                     <th></th>
                                  </tr>
                              </thead>
                              <tbody>
@@ -69,6 +73,18 @@
                                              class="btn btn-sm btn-primary jedatombol"
                                              title="Riwayat Service <?= $kend['merk'] . ' ' . $kend['tipe'] . ' ' . $kend['no_polisi'] ?>"><i
                                                  class="fa fa-tools"></i></a>
+                                         <?php if ($this->session->userdata('role') != "Admin") : ?>
+                                         <a href="<?= site_url('admin/pagu?id=' . $kend['idk'] . '') ?>"
+                                             class="btn btn-sm btn-dark jedatombol"
+                                             title="Pagu Tahunan Kendaraan <?= $kend['merk'] . ' ' . $kend['tipe'] . ' ' . $kend['no_polisi'] ?>"><i
+                                                 class="fa fa-wallet"></i></a>
+                                         <a href="<?= site_url('home/print_data_kendaraan?id=' . $kend['idk'] . '') ?>"
+                                             class="btn btn-sm btn-dark jedatombol"
+                                             title="Print Data Kendaraan <?= $kend['merk'] . ' ' . $kend['tipe'] . ' ' . $kend['no_polisi'] ?>"><i
+                                                 class="fa fa-print"></i></a>
+                                         <?php endif ?>
+                                     </td>
+                                     <td class="text-center">
                                          <a href="<?= site_url('home/riwayat_bbm?id=' . ($kend['idk']) . '') ?>"
                                              class="btn btn-sm btn-primary jedatombol"
                                              title="Riwayat BBM <?= $kend['merk'] . ' ' . $kend['tipe'] . ' ' . $kend['no_polisi'] ?>"><i
@@ -78,18 +94,10 @@
                                              title="Riwayat Pajak Kendaraan <?= $kend['merk'] . ' ' . $kend['tipe'] . ' ' . $kend['no_polisi'] ?>"><i
                                                  class="fa fa-align-justify"></i></a>
                                          <?php if ($this->session->userdata('role') != "Admin") : ?>
-                                         <a href="<?= site_url('admin/pagu?id=' . $kend['idk'] . '') ?>"
-                                             class="btn btn-sm btn-dark jedatombol"
-                                             title="Pagu Tahunan Kendaraan <?= $kend['merk'] . ' ' . $kend['tipe'] . ' ' . $kend['no_polisi'] ?>"><i
-                                                 class="fa fa-wallet"></i></a>
                                          <a href="<?= site_url('home/pengajuan_servis?id=' . $kend['idk'] . '') ?>"
                                              class="btn btn-sm btn-info jedatombol"
                                              title="Pengajuan Servis Kendaraan <?= $kend['merk'] . ' ' . $kend['tipe'] . ' ' . $kend['no_polisi'] ?>"><i
                                                  class="fa-solid fa-ballot"></i></a>
-                                         <a href="<?= site_url('home/print_data_kendaraan?id=' . $kend['idk'] . '') ?>"
-                                             class="btn btn-sm btn-dark jedatombol"
-                                             title="Print Data Kendaraan <?= $kend['merk'] . ' ' . $kend['tipe'] . ' ' . $kend['no_polisi'] ?>"><i
-                                                 class="fa fa-print"></i></a>
                                          <a onclick="editConfirm('<?= site_url('home/edit_kendaraan?id=' . $kend['idk'] . '') ?>')"
                                              href="#" class="btn btn-sm btn-warning jedatombol"
                                              title="Edit Data Kendaraan <?= $kend['merk'] . ' ' . $kend['tipe'] . ' ' . $kend['no_polisi'] ?>"><i
